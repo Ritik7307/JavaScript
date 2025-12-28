893dce0410f3b0f70adf2495f31e2d5fe8a6b1e9
Ritik7307 <vijayshankarprajapati29@gmail.com>
2025-12-28 14:48:43 +0530

    initial

---
+++ 2_variables.js
@@ -0,0 +1,9 @@
+const accountId = 14417;
+let accountEmail = 'ritik@gmail.com'
+var accountPass = 'wfh@gdu'
+let accountCity = 'Jaipur'
+let accountState = 'Rajasthan'
+/*Prefer not to use
+var because of issue in block scope and functional scope */
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
---
+++ 3_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat al JS code as newer version
+
+// alert(4+6) we areusing nodejs , not browser
+
+
+/*
+number =>2 to power 53
+bigInt
+string = " "
+boolean : true/false
+null : standalone value
+undefined
+symbol
+ */
+
+// object
+// arrray
+// function
